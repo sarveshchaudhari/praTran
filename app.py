a96f2d2429ec351c7b97bea1ead2c2e803cd5171
sarveshchaudhari <chaudharisarvesh10@gmail.com>
2024-10-18 01:13:31 +0530

    Created flask application, loaded the model using hugginface safetensors, added html translate page, added styles, made website responsive

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+from translation_model import translate
+import time
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/translate', methods=['POST'])
+def translate_text():
+    start_time = time.time()
+    input_text = request.form['prakrit_text']
+    translated_text = translate(input_text)
+    translation_time = time.time() - start_time
+    return render_template('index.html', original_text=input_text, translated_text=translated_text, time_taken=translation_time)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ translation_model.py
@@ -0,0 +1,21 @@
+import time
+from transformers import pipeline
+
+# Load the translation model at the start to avoid reloading on each request
+start_time = time.time()
+pipe = pipeline("text2text-generation", model="model")
+print(f"Model loaded in {time.time() - start_time} seconds")
+
+def translate(input_text):
+    print(f"Input text: {input_text}")
+    output_text = pipe(
+        input_text,
+        do_sample=True,
+        num_beams=5,
+        max_length=50,
+        no_repeat_ngram_size=2,
+        temperature=0.7
+    )
+    translated_text = output_text[0]['generated_text']
+    print(f"Output text: {translated_text}")
+    return translated_text
